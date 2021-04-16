a102edf2021e2d57b5a07f8f48208ecf596d179d
Fernando Francisco de Oliveira <fernando@oliveira.eti.br>
2021-04-16 14:59:48 -0300

    Fixes because the incorrect merge

--- src/Cofoundry.Domain/Domain/Users/Commands/LogFailedLoginAttemptCommandHandler.cs
+++ src/Cofoundry.Domain/Domain/Users/Commands/LogFailedLoginAttemptCommandHandler.cs
@@ -4,15 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Cofoundry.Domain.CQS;
 using Cofoundry.Core.EntityFramework;
-<<<<<<< HEAD
 using Cofoundry.Core;
 using Cofoundry.Domain.Data;
 using MySql.Data.MySqlClient;
-=======
-using Microsoft.Data.SqlClient;
-using Cofoundry.Core;
-using Cofoundry.Domain.Data;
->>>>>>> 6ecdeb969200643b332b1c86e2aba97ab0ff9ce6
 
 namespace Cofoundry.Domain.Internal
 {
@@ -44,17 +38,10 @@ namespace Cofoundry.Domain.Internal
 
             await _sqlExecutor.ExecuteCommandAsync(_dbContext,
                 "Cofoundry.FailedAuthticationAttempt_Add",
-<<<<<<< HEAD
                 new MySqlParameter("UserAreaCode", command.UserAreaCode),
                 new MySqlParameter("Username", TextFormatter.Limit(command.Username, 150)),
                 new MySqlParameter("IPAddress", connectionInfo.IPAddress),
                 new MySqlParameter("DateTimeNow", executionContext.ExecutionDate)
-=======
-                new SqlParameter("UserAreaCode", command.UserAreaCode),
-                new SqlParameter("Username", TextFormatter.Limit(command.Username, 150)),
-                new SqlParameter("IPAddress", connectionInfo.IPAddress),
-                new SqlParameter("DateTimeNow", executionContext.ExecutionDate)
->>>>>>> 6ecdeb969200643b332b1c86e2aba97ab0ff9ce6
                 );
         }
     }
